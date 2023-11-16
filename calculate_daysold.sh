ba842e7da176e9a5b71a88c3c838a98193a192a8
freender <40869255+freender@users.noreply.github.com>
2023-11-15 20:30:19 -0500

    Update calculate_daysold.sh

--- calculate_daysold.sh
+++ calculate_daysold.sh
@@ -4,7 +4,7 @@
 ## https://github.com/freender/mover_tunning_daysold/blob/main/calculate_daysold.sh
 
 # Retention Variables
-target_space="400000000000" #Set how many free space should be available after mover runs. 400 GB by default
+target_space="500000000000" #Set how many free space should be available after mover runs. 400 GB by default
 
 # Share variables:
 ignore_file='/mnt/user/backup/mover-ignore/mover_ignore.txt' #File with list of directories ignored by mover
@@ -15,15 +15,20 @@ share_config='/boot/config/plugins/ca.mover.tuning/shareOverrideConfig/media.cfg
 # Functions
 # This function calculates current free space on share
 get_free_space() {
-    free_space=`df $share_path | awk 'NR>1{print $4*1000}'`
+  free_space=`df $share_path | awk 'NR>1{print $4*1000}'`
 }
 # This function calculates how many space is estimated to be reclaimed
 get_reclaimable_space() {
-    to_be_reclaimed=`find $share_path ! -mtime -$daysold -type f -printf "%s %p\n" | grep -vFf "$ignore_file" | awk '{s+=$1} END {print s}'`
+  to_be_reclaimed=`find $share_path ! -mtime -$daysold -type f -printf "%s %p\n" | grep -vFf "$ignore_file" | awk '{s+=$1} END {print s}'`
+  to_be_reclaimed=${to_be_reclaimed:="0"}
 }
 # This function updates daysold in config file
 update_share_config(){
-    sed -i -e 's/daysold=.*/daysold="'"$daysold"'"/g' $share_config
+  # Update config only if daysold changed 
+  if [[ $initial_retention -ne $daysold ]]
+    then 
+      sed -i -e 's/daysold=.*/daysold="'"$daysold"'"/g' $share_config
+    fi 
 }
 
 # Main Script
@@ -34,31 +39,24 @@ initial_retention=$daysold
 get_free_space
 get_reclaimable_space
 
-# Check if mover should be executed today
+# Increase $daysold if enough space available
 if [ $free_space -gt $target_space ]
 then
-  if [[ "$to_be_reclaimed" == "" ]] #In this case no file were created before retention period and enough free space available 
-    then
-    echo "Mover is not required. Share:" $share_path
-    exit 1
-  else
-  daysold=$((daysold+5))
+    while [[ "$to_be_reclaimed" -ne 0 ]] && [[ "$daysold" -lt 365  ]] ; do 
+      daysold=$((daysold+5))
+      get_reclaimable_space
+    done
   update_share_config
-  echo "Mover is not required. Retention has been extended by 5 days. Share:" $share_path
+  echo "Free space more then target space. Retention period has been updated to" $daysold "days. Share:" $share_path
   exit 1
-  fi
-fi  
+fi 
 
-# If should - calculate new retention period
-while [[ $(($free_space + $to_be_reclaimed)) -lt $target_space ]] && [[ "$to_be_reclaimed" -gt 0  ]] ; do 
+# Decrease $daysold if not enough space available
+while [[ $(($free_space + $to_be_reclaimed)) -lt $target_space ]] && [[ "$daysold" -gt 0  ]] ; do 
   daysold=$((daysold-5))
   get_reclaimable_space
 done
 
-# Update config only if daysold changed 
-if [[ $initial_retention -ne $daysold ]]
-then update_share_config
-  update_share_config 
-fi 
-
-echo "Share:" $share_path "Calculated Retention=" $daysold " Estimated Free Space=" $to_be_reclaimed
+update_share_config 
+ 
+echo "Share:" $share_path "Calculated Retention=" $daysold " To be reclaimed=" $to_be_reclaimed
